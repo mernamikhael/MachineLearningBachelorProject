ca9d5a5ec3ffdfbde1b697fb81075da824e16a2b
mernamikhael <merna.mikhael@student.guc.edu.eg>
2018-07-05 08:49:25 +0200

    test main

--- test.py
+++ test.py
@@ -1,131 +1,115 @@
-from scipy.stats import beta
-import matplotlib.pyplot as plt
-import numpy as np
-import user
-import userProf1
-import userprof2
-import userProf3
-import weight
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
+import json
+import time
+import tweepy
 import csv
-from scipy.optimize import fsolve
-import uncertainties as u
+# from test.test_decimal import init
+from tweepy import OAuthHandler
+from tweepy import API
+from tweepy import Cursor
+from datetime import datetime, date, time, timedelta
+from collections import Counter
+import sys
 
-user_id=[]
-userOSAND=[]
-UserOSIOS=[]
-userAOSAND=[]
-UserAOSIOS=[]
-font = {'family': 'serif',
-        'color':  'darkred',
-        'weight': 'normal',
-        'size': 16,
-        }
-with open('dataset\users.csv', 'rb') as fil:
-    t = csv.reader(fil)
-    for row in t:
-        user_id.append(row[2]) 
-        userOSAND.append(row[4])
-        UserOSIOS.append(row[5])
-index=4
-# while (index <47):
-print index
-# weightTopic,weightLang,weightpost,weighttag,weightfre=weight.weight(index,user_id,userOSAND,UserOSIOS)
-weightTopic=3
-weightLang=1
-weightpost=2
-weighttag=4
-weightfre=5
-print weightTopic,weightLang,weightpost,weighttag,weightfre
-alpha3,beta3,alpha2,beta2,alpha1,beta1=userProf1.user(user_id[index], index,weightTopic,weightLang,weightpost,weighttag,weightfre)
+access_token = "1012234479832895488-B8LBfisf1FvgnnPuHigo98434GxoXs"
+access_token_secret = "KB1TFhcC4H9NcjwkbHnE7C05vsc90qu9VZZLKTHDXtlkK"
+consumer_key = "xrqojmbzrzRqR1BXV9ORp75Zt"
+consumer_secret = "lhhNNw77O534Ck1yXcbUKJ76km6h5y6lgF96SE3S4EPmLjfjj2"
 
-i=0
-x = np.linspace(0, 1.0, 100)
-
-week1 = beta.pdf(x, alpha1,beta1) 
-mean2, var, skew, kurt = beta.stats( alpha1,beta1, moments='mvsk')
-print mean2
-
-week2 = beta.pdf(x,alpha2,beta2) 
-mean3, var, skew, kurt = beta.stats( alpha2,beta2, moments='mvsk')
-print mean3
-
-week3 = beta.pdf(x,alpha3,beta3) 
-mean4, var, skew, kurt = beta.stats( alpha3,beta3, moments='mvsk')
-print mean4
-
-while(i<len(user_id)):
-    indexA=i
-    alphaHack,betaHack= user.user(user_id[index], user_id[indexA],index,indexA,userOSAND[index],UserOSIOS[index],userOSAND[indexA],UserOSIOS[indexA],weightTopic,weightLang,weightpost,weighttag,weightfre)
-
-    print alphaHack,betaHack
-
-    # hack
-    hack = beta.pdf(x, alphaHack,betaHack) 
-    mean1, var, skew, kurt = beta.stats(alphaHack,betaHack, moments='mvsk')
-    print mean1
-    plt.plot(x,hack)
-    plt.plot(x,week3)
-    plt.plot(x,week2)
-    plt.plot(x,week1)
-    # week1 normal
-
-    plt.xlabel('User Behavior Variation \n a= '+str(alphaHack)+'b= '+str(betaHack), font)
-    plt.ylabel('Beta PDF', font)
-    plt.savefig("wgraph/"+str(index)+"vs"+str(indexA)+".png")
-    # # plotting code
-    # plt.show()
-    plt.clf()
-    # print fsolve(lambda x : hack - week3,0.0)
-
-    # # plotting code
-    # plt.show()
-
-    # detect most weird behavior of user
-    hack=0
-    # d1=mean3-mean2
-    # d2=mean4-mean3
-    # d3=mean1-mean4
-    V1=0.5-mean2
-    V2=0.5-mean3
-    v3=0.5-mean4
+class StdOutListener(StreamListener):
     
-    averageV= (V1+v3+V2)/3.0
-    vH=0
-    if(mean1>0.5):
-        hack=1
-    else:
-        vH=0.5-mean1
-    if(mean3==mean2 and mean2==mean4 and mean2<0.05):
-        level=averageV*0.8
-    else:
-        level=averageV*0.75
+    def on_data(self, data):
+        #x = json.loads(data)
+        #if(not isReply(x)):
+        print data
+        return True
 
-    # if(d2>=0):
-    #     level-=(d2)
-    # else:
-    #     level+=(d2)
-    # if(d1>=0):
-    #     level-=(d1)
-    # else:
-    #     level+=(d1)
-    # # if very static user
-    # if(d2==0 and d1==0):
-    #     level+=(mean2*5)
-    # com=0.5
-    # if(mean4<0.5):
-    #     com-=(d3)
-    # else:
-    #     com+=(d3)
-            
-    if(vH<=level):
-        hack=1
-    # if( vH<=0):
-    #     hack=0
-    print i , hack , level ,vH
+    def on_error(self, status):
+        print status
 
-    i+=1
-    # index+=1
-   
+if __name__ == '__main__':
 
-    
+    #This handles Twitter authentication and the connection to Twitter Streaming API
+    l = StdOutListener()
+    auth = OAuthHandler(consumer_key, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
+    api = tweepy.API(auth)
+    # 200 tweets to be extracted
+    number_of_tweets=200
+    idlevel=[]
+    mainID=[]
+
+    with open("id university of south carolina.csv","rb") as f:
+        t=csv.reader(f)
+        for row in t:
+            for k in row:
+                idlevel.append(k)
+                mainID.append(idlevel)
+                try:
+                    u = api.get_user(k)
+                    screen_name=u.screen_name
+                    print k,screen_name
+                    levelout= csv.writer(open("Dataset/ids.csv","wb"),delimiter=(','))
+                    levelout.writerow(mainID)
+                    idlevel=[]
+                    mainID=[]
+                
+                    tweets= Cursor(api.user_timeline, id=k).items()
+                    if(tweets):
+                        out= csv.writer(open("Dataset/tweets_"+k+"_"+screen_name+".csv","wb"),delimiter=(','))
+                        a=[]
+                        b=[]
+                        hashtags = []
+                        mentions = []
+                        urls = []
+                        tweet_count = 0
+                        # end_date = datetime.utcnow() - timedelta(days=30)
+                        for status in tweets:
+                            a.append(status.text.encode('utf-8'))
+                            a.append(" ")
+                            a.append(status.id)
+                            a.append(status.created_at)
+                            a.append(" ")
+                            tweet_count += 1
+                            if hasattr(status, "entities"):
+                                entities = status.entities
+                                if "hashtags" in entities:
+                                    for ent in entities["hashtags"]:
+                                        if ent is not None:
+                                            if "text" in ent:
+                                                hashtag = ent["text"]
+                                                if hashtag is not None:
+                                                    hashtags.append(hashtag)
+                                if "user_mentions" in entities:
+                                    for ent in entities["user_mentions"]:
+                                        if ent is not None:
+                                            if "screen_name" in ent:
+                                                name = ent["screen_name"]
+                                                if name is not None:
+                                                    mentions.append(name)
+                                if "urls" in entities:
+                                    for url in entities["urls"]:
+                                        if url is not None:
+                                            # urls.append(url)
+                                            if "expanded_url" in url:
+                                                expUrl = url["expanded_url"]
+                                                if expUrl is not None:
+                                                    urls.append(expUrl)
+                            
+                            a.append(mentions)
+                            a.append(hashtags)
+                            a.append(status.lang)
+                            a.append(urls)
+                            b.append(a)
+                            out.writerows(b)
+                            a=[]
+                            b=[]
+                            mentions= []
+                            hashtags= []
+                except:
+                    
+                #     print "not authori"
+                    continue
